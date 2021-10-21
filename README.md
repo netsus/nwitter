6caeb254bd85c0123c603943f0c4b8493ddc6794
netsus <cwjcsk@naver.com>
2021-10-22 01:54:53 +0900

    #1.0 React + Firebase Setup sudo npm install -g firebase-tools 설치로 변경 npm run start 부분이 작동되지 않음 index.js에서 Module not found 에러 발생

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ firebase를 이용해 twitter 클론 코딩하기
 
 1. create-react-app : npx create-react-app nwitter
 2. firebase 회원가입 및 nwitter 프로젝트 생성
-  1) firebase cli 다운로드 : npm install firebase@8.10.0
+  1) firebase cli 다운로드 : sudo npm install -g firebase-tools 
   
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,4 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
