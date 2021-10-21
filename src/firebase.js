61ba52cee4cd0db41141c4430618c57a081c9116
netsus <cwjcsk@naver.com>
2021-10-22 01:17:13 +0900

    #1.0 React + Firebase Setup firebase 회원가입 및 nwitter 프로젝트 생성 -> SDK 확인 1. npm install firebase@8.10.0 다운로드

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByVXwhIijKJW3bD-z0L3Q20iIn6VnxqO8",
+  authDomain: "nwitter-8e536.firebaseapp.com",
+  projectId: "nwitter-8e536",
+  storageBucket: "nwitter-8e536.appspot.com",
+  messagingSenderId: "107219779961",
+  appId: "1:107219779961:web:bf54d58449c7899b196fa8"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
